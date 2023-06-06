a74a90c56f5717f06b34604fe41f4ee2423fb9be
namiqabasov <7td508c@.code.az>
2023-06-06 13:28:01 +0400

    code-project.06.06.2023

---
+++ my-project/assets/js/slick.js
@@ -0,0 +1,15 @@
+$(".fade").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
+  autoplaySpeed: 2000,
+});
+
+const slicknext = (document.querySelector(
+  ".slick-next"
+).innerHTML = `<i class="fa-solid fa-chevron-right"></i>`);
+const slickprev = (document.querySelector(
+  ".slick-prev"
+).innerHTML = `<i class="fa-solid fa-chevron-left"></i>`);
