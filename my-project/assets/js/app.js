5637f4ec5fcdb97485af749a147f1ad3a7304a45
namiqabasov <7td508c@code.edu.az>
2023-06-11 09:45:44 +0400

    code-project.11.06.2023

--- my-project/assets/js/app.js
+++ my-project/assets/js/app.js
@@ -59,12 +59,59 @@ async function myfetch1() {
   const respon = await fetch(`http://localhost:3000/post`);
   const data = await respon.json();
 
-  data.forEach((element) => {
+  const nevdata = await data.forEach((element) => {
     cretElement1(element);
+  });
+
+  $(".autoplay").slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 380,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 }
 const mycards = document.querySelector(".mycards");
-const autoplay = document.querySelector(".card");
+const autoplay = document.querySelector(".autoplay");
 function cretElement(data) {
   // createelement
   const carddiv = document.createElement("div");
@@ -140,8 +187,7 @@ function cretElement1(data) {
 
   // classname
   carddiv.classList.add("card");
-  carddiv.classList.add("slick-slide");
-  carddiv.classList.add("slick-cloned");
+
   imgdiv.classList.add("img");
   shopdiv.classList.add("shop");
   btndiv.classList.add("divbtn");
