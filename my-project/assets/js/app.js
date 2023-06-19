2f12ae3ee9754a94444a5cca9597c5e60e486b74
namiqabasov <7td508c@code.edu.az>
2023-06-19 23:33:23 +0400

    code-project 19.06.2023

--- my-project/assets/js/app.js
+++ my-project/assets/js/app.js
@@ -269,6 +269,7 @@ function cretElement(data) {
 
   // classname
   carddiv.classList.add("card");
+  carddiv.classList.add("cardfillret");
   imgdiv.classList.add("img");
   shopdiv.classList.add("shop");
   btndiv.classList.add("divbtn");
@@ -419,7 +420,7 @@ const showall = document.querySelector(".showall");
 //filter ucun
 
 man_btn.addEventListener("click", function () {
-  document.querySelectorAll(".card").forEach((element) => {
+  document.querySelectorAll(".cardfillret").forEach((element) => {
     element.style.display = "none";
   });
   document.querySelectorAll(".man").forEach((element) => {
@@ -427,7 +428,7 @@ man_btn.addEventListener("click", function () {
   });
 });
 woman_btn.addEventListener("click", function () {
-  document.querySelectorAll(".card").forEach((element) => {
+  document.querySelectorAll(".cardfillret").forEach((element) => {
     element.style.display = "none";
   });
   document.querySelectorAll(".woman").forEach((element) => {
@@ -435,7 +436,7 @@ woman_btn.addEventListener("click", function () {
   });
 });
 child_btn.addEventListener("click", function () {
-  document.querySelectorAll(".card").forEach((element) => {
+  document.querySelectorAll(".cardfillret").forEach((element) => {
     element.style.display = "none";
   });
   document.querySelectorAll(".child").forEach((element) => {
@@ -443,12 +444,12 @@ child_btn.addEventListener("click", function () {
   });
 });
 showall.addEventListener("click", function () {
-  document.querySelectorAll(".card").forEach((element) => {
+  document.querySelectorAll(".cardfillret").forEach((element) => {
     element.style.display = "block";
   });
 });
 hous_btn.addEventListener("click", function () {
-  document.querySelectorAll(".card").forEach((element) => {
+  document.querySelectorAll(".cardfillret").forEach((element) => {
     element.style.display = "none";
   });
   document.querySelectorAll(".house").forEach((element) => {
